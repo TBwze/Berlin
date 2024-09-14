f5cb0a19184bc784ecaa71298dee9614ba61efa4
Kenny Prasetia <kenny.prasetya03@gmail.com>
2024-09-14 23:18:12 +0700

    change card component name

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import CardComponent from "../components/Card.componentt";
+import CardComponent from "../components/Card.component";
 
 const Home = () => {
   const form = useForm({
