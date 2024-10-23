1db8763e4b05948e59f3b22f0be09281424e56a3
Christ-edmund23 <94790146+Christ-edmund23@users.noreply.github.com>
2024-10-23 09:52:12 +0700

    Home button (Not Authorize)

--- client/src/pages/NotAuthorize.jsx
+++ client/src/pages/NotAuthorize.jsx
@@ -1,7 +1,10 @@
 import Unauthorize from "../assets/Unauthorize.png"
+import { useNavigate } from "react-router-dom";
 import CustomButton from '../components/CustomButton.component';
 
 const NotAuthorizePage = () => {
+    const navigate = useNavigate();
+
     return (
       <div className="flex flex-row items-center justify-center mx-auto max-w-[1280px] p-4">
         <img src={Unauthorize} alt="Unauthorize Logo" className="w-1/3"/>
@@ -15,7 +18,7 @@ const NotAuthorizePage = () => {
             bgColor="#4169E1"
             styles="font-semibold rounded px-4 border-2"
             textColor="#ffffff"
-            handleClick={() => navigate("/Home")}
+            handleClick={() => navigate("/")}
           />
           </div>
         </div>
